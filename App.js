136c1a0dec874e3ac25f266e91b8fe9ae3d77998
Pinti <pintimarius031@outlook.com>
2024-04-16 00:16:52 +0300

    APP.js updates

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [enteredGoalText, setEnteredGoalText] = useState('');
+    const [yourGoals, setYourGoals] = useState([]);
+
+    function goalInputHandler (enteredText) {
+        setEnteredGoalText(enteredText);
+    }
+
+    function addGoalHandler () {
+        setYourGoals((currentGoals) => [
+            ...yourGoals,
+            enteredGoalText
+        ]);
+    }
+
+
+    return (
+        <View style={styles.appContainer}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder='Type your goal!'
+                    onChangeText={goalInputHandler}
+                />
+                <Button title="Add Goal" onPress={addGoalHandler}/>
+            </View>
+            <View style={styles.goalsContainer}>
+                {yourGoals.map((goal) => (<Text key={goal}>{goal}</Text>))}
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    appContainer: {
+        flex: 1,
+        paddingTop: 50,
+        paddingHorizontal: 16
+    },
+    inputContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 24,
+        borderBottomWidth: 1,
+        borderBottomColor: '#cccccc'
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        width: '70%',
+        marginRight: 8,
+        padding: 8
+    },
+    goalsContainer: {
+        flex: 5
+    }
+
 });
